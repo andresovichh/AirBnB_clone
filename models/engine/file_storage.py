d99790e7ed3036e6ed75c25841fb4e1b75b57ea5
andresovichh <ahenderson.holberton@gmail.com>
2022-02-28 15:30:28 -0300

    trying filestorage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import json
+from models.base_model import BaseModel
+
+class Filestorage(BaseModel):
+    """ Class that serializes instances to a JSON
+    file and deserializes JSON files to instances"""
+    
+    def __init__(self, filepath, emptydict):
+        Filestorage.__file_path = filepath
+        Filestorage.__objects = emptydict
+
+    def all(self):
+        """ returns the dictionary __objects """
+        return self.__objects.__dict__
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id"""
+        algo con setattr
+    
+    def save(self):
+        """  serializes __objects to the JSON file (path: __file_path)"""
+        from python to json
+    
+    def reload(self):
+        """ deserializes the JSON file to __objects 
+        (only if the JSON file (__file_path) exists 
+        otherwise, do nothing. If the file doesn
+        t exist, no exception should be raised)"""
+        
+
+
