4c56c856be4c286549b82a96a8014bcc9809ccb5
Diego Monfort <diegomonfort12346@gmail.com>
2022-03-04 05:21:06 -0800

    [TEST STRING]

---
+++ tests/test_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Test State File """
+import unittest
+from models.state import State
+
+
+class Test_State(unittest.TestCase):
+    """ Test State Class """
+
+    def test_name_string(self):
+        """ Test not a single character """
+        self.assertEqual(type(State().name), str)
