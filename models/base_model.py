82c207a929262a70b019cb8f794f83e25c0b010a
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 20:58:30 -0300

    test

--- models/base_model.py
+++ models/base_model.py
@@ -38,7 +38,7 @@ class BaseModel:
 
     def __str__(self):
         """ should print: [<class name>] (<self.id>) <self.__dict__> """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
+        return "[{}] ({}) {}\t".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
