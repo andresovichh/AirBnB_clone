0453b5096e358bc7831bdd50122726dba6d68e1c
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 20:17:32 -0300

    pep8 unittest

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -4,6 +4,7 @@
 
 import json
 import unittest
+import pep8
 from models.engine.file_storage import FileStorage
 import os
 from datetime import datetime
@@ -41,3 +42,20 @@ class TestFilestorage(unittest.TestCase):
         """documentation"""
 
         self.assertTrue(len(FileStorage.reload.__doc__) > 0)
+
+class TestCodeFormat(unittest.TestCase):
+    def test_pep8_conformance(self):
+        # """Test that we conform to PEP8."""
+        # pep8style = pep8.StyleGuide(quiet=True)
+        # result = pep8style.check_files(['base.py'])
+        # print(result)
+        # self.assertEqual(result.total_errors, 0,
+        #                  "Found code style errors (and warnings).")
+        # https://pep8.readthedocs.io/en/release-1.7.x/intro.html
+        fchecker = pep8.Checker('models/engine/file_storage.py', show_source=True)
+        file_errors = fchecker.check_all()
+
+        print("Found %s errors (and warnings)" % file_errors)
+
+if __name__ == "__main__":
+    unittest.main()
