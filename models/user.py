78b557abbe55741013ca824d7ced3f3819323b44
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 13:49:35 -0300

    test

--- models/user.py
+++ models/user.py
@@ -5,7 +5,9 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ Create User """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self):
+        """ initialization of USER"""
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
