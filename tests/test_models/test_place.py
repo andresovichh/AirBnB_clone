8235b3d93fac3679f306b4d43932b0543ade2443
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 14:02:35 -0300

    test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Test State File """
 from models.place import Place
+from models.amenity import Amenity
 import unittest
 
 
@@ -12,3 +13,22 @@ class Test_Place(unittest.TestCase):
         """documentation"""
 
         self.assertTrue(len(Place.__doc__) > 0)
+
+#!/usr/bin/python3
+""" Test State File """
+from models.place import Place
+import unittest
+
+
+
+class Test_Amenity(unittest.TestCase):
+    """ Test State Class """
+
+    def test_documentation(self):
+        """documentation"""
+
+        self.assertTrue(len(Amenity.__doc__) > 0)
+    def test_documentation(self):
+        """documentation"""
+
+        self.assertTrue(len(Amenity.__init__.__doc__) > 0)
