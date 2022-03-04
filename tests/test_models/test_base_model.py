f8c1a7cd0db2984d538717b0a85a7b28f4416606
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 20:20:41 -0300

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,9 +4,8 @@
 
 import json
 import unittest
-from unittest.mock import Base
+import pep8
 from models.base_model import BaseModel
-import os
 from datetime import datetime
 
 
@@ -65,3 +64,20 @@ class TestBaseModel(unittest.TestCase):
     #     """ check for non existing class"""
     #    my_model = something()
     #    self
+
+class TestCodeFormat(unittest.TestCase):
+    def test_pep8_conformance(self):
+        # """Test that we conform to PEP8."""
+        # pep8style = pep8.StyleGuide(quiet=True)
+        # result = pep8style.check_files(['base.py'])
+        # print(result)
+        # self.assertEqual(result.total_errors, 0,
+        #                  "Found code style errors (and warnings).")
+        # https://pep8.readthedocs.io/en/release-1.7.x/intro.html
+        fchecker = pep8.Checker('models/base_model.py', show_source=True)
+        file_errors = fchecker.check_all()
+
+        print("Found %s errors (and warnings)" % file_errors)
+
+if __name__ == "__main__":
+    unittest.main()
