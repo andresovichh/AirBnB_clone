c7c0691a1dcf5affe549f69e88a4058e41fb9788
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 13:23:23 -0300

    test

--- tests/tests_models/test_file_storage.py
+++ tests/tests_models/test_file_storage.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python3
-""" TEst FileStorage """
+#!/usr/bin/env python3
+
+""" unittest for BaseModel Class"""
+
+import json
 import unittest
 from models.engine.file_storage import FileStorage
+import os
+from datetime import datetime
 
 
-# class Test_FileStorage(unittest.TestCase):
-#     """ Test FileStorage """
+class TestBase(unittest.TestCase):
+    """ initial test class"""
+    # 1. Test if documentation present
+    def test_documentation(self):
+        """documentation"""
 
-#     def test_new(self):
-#         """ Test if key is correct """
-#         self.assertEqual(FileStorage.obj, "BaseModel.12121212")
+        self.assertTrue(len(FileStorage.__doc__) > 0)
