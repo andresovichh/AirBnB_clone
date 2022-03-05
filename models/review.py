362077d13b075733e14ff8a85e8c7a2134d36b04
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 21:34:51 -0300

    removed __init__  from attributes

--- models/review.py
+++ models/review.py
@@ -10,8 +10,6 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """ a Review class definition that inherits
     from BaseModel"""
-    def __init__(self):
-        """ a Review object instantiation"""
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
+    place_id = ""
+    user_id = ""
+    text = ""
