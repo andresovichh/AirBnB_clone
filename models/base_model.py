ad82ab99521bc3c9a108042f97af51ebd7eeab37
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 22:32:09 -0300

    test

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ Console for AirBnb Proyect
 
 PLEASE CHECK THAT: storage is instanciated at
 __init__.py file located in models
-"""
+ """
 
 import json
 from datetime import datetime
@@ -38,7 +38,7 @@ class BaseModel:
 
     def __str__(self):
         """ should print: [<class name>] (<self.id>) <self.__dict__> """
-        return "\t[{}] ({}) {}".format(self.__class__.__name__,
+        return "[{}] ({}) {}\t".format(self.__class__.__name__,
                                        self.id, self.__dict__)
 
     def save(self):
