5789ed612601c1658ab5b06d1de7a40244dbd498
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 21:54:49 -0300

    test

--- models/amenity.py
+++ models/amenity.py
@@ -9,3 +9,6 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """ a class definition of Amenity"""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -10,3 +10,6 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ A class State that inherits from BaseModel"""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
