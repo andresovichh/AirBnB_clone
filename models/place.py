31e89fb75789c8ff5e2a2632d1bbf92dba252bf9
andresovichh <ahenderson.holberton@gmail.com>
2022-03-04 22:02:39 -0300

    "r" was mising in price_by_night

--- models/place.py
+++ models/place.py
@@ -16,7 +16,7 @@ class Place(BaseModel):
     number_rooms = 0
     number_bathrooms = 0
     max_guest = 0
-    pice_by_night = 0
+    price_by_night = 0
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
